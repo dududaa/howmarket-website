8d95c62c2a1a57a834d615a142993048d65c5a64
Ajayi Olamide <prodbyola@gmail.com>
2026-01-15 11:50:03 +0100

    collecting form data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// show drawe
 const drawerTogglers = document.getElementsByClassName("drawerToggle")
 for (let i = 0; i < drawerTogglers.length; i++) {
     const toggler = drawerTogglers.item(i)
@@ -9,9 +10,30 @@ for (let i = 0; i < drawerTogglers.length; i++) {
     }
 }
 
+// hide drawer
 const drawer = document.getElementById('drawer')
 if(drawer){
     drawer.addEventListener('click', () => {
         drawer.style.display = 'none'
     })
+}
+
+// submit form
+const form = document.getElementById('contact-form')
+if(form){
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const name = getInputValue('user-name')
+        const contact = getInputValue('user-contact')
+        const reason = getInputValue('user-reason')
+        const message = getInputValue('user-message')
+
+        const data = { name, contact, reason, message }
+        console.log(data)
+    })
+}
+
+const getInputValue = (inputId) => {
+    return document.getElementById(inputId).value
 }
