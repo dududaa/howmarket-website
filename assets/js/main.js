1b8195cb379e3a58311ab30ab399f24ed687f284
Ajayi Olamide <prodbyola@gmail.com>
2026-01-15 14:12:16 +0100

    sending contact message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,31 @@
-// show drawe
+// show drawer
 const drawerTogglers = document.getElementsByClassName("drawerToggle")
 for (let i = 0; i < drawerTogglers.length; i++) {
     const toggler = drawerTogglers.item(i)
     if (toggler) {
         toggler.addEventListener('click', () => {
             const drawer = document.getElementById('drawer')
-            if(drawer) drawer.style.display = 'block'
+            if (drawer) drawer.style.display = 'block'
         })
     }
 }
 
 // hide drawer
 const drawer = document.getElementById('drawer')
-if(drawer){
+if (drawer) {
     drawer.addEventListener('click', () => {
         drawer.style.display = 'none'
     })
 }
 
 // submit form
+const SUPABASE_URL = 'https://lwowlfiwudhwyssgkopy.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_vAezcaqeY6B9tZ-dgj-i2w_iyjtjZZd';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 const form = document.getElementById('contact-form')
-if(form){
-    form.addEventListener('submit', (e) => {
+if (form) {
+    form.addEventListener('submit', async (e) => {
         e.preventDefault()
 
         const name = getInputValue('user-name')
@@ -31,6 +35,13 @@ if(form){
 
         const data = { name, contact, reason, message }
         console.log(data)
+        const { error } = await client
+            .from('contact_messages')
+            .insert([data]);
+
+        if (error) {
+            console.log(error)
+        }
     })
 }
 
